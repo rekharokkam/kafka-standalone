603cfe7b72b4d6ebe61f1d5f3c8287cb7184ca04
rrokkam <rekha.rokkam@target.com>
2021-08-13 20:59:16 -0700

    added example for CustomerComplex Object

--- kafka-avro/src/main/java/com/learning/spring/kafka/avro/SpecificRecordCustomerBasicExample.java
+++ kafka-avro/src/main/java/com/learning/spring/kafka/avro/SpecificRecordCustomerBasicExample.java
@@ -12,10 +12,10 @@ import java.io.File;
 import java.io.IOException;
 
 @Slf4j
-public class SpecificRecordExample {
+public class SpecificRecordCustomerBasicExample {
 
     public static void main(String[] args) {
-        //Create SpecificRecord
+        //Create SpecificRecord for Customer Basic
         CustomerBasic.Builder customerBasicBuilder = CustomerBasic.newBuilder();
         customerBasicBuilder.setFirstName("Vidyut");
         customerBasicBuilder.setLastName("Jammwal");
@@ -30,7 +30,7 @@ public class SpecificRecordExample {
         //Write to a file
         final DatumWriter<CustomerBasic> datumWriter = new SpecificDatumWriter<>(CustomerBasic.class);
         try (DataFileWriter<CustomerBasic> dataFileWriter = new DataFileWriter<>(datumWriter)) {
-            dataFileWriter.create(customerBasic.getSchema(), new File("customer-specific-generated.avro"));
+            dataFileWriter.create(customerBasic.getSchema(), new File("customer-basic-specific-generated.avro"));
             dataFileWriter.append(customerBasic);
             log.info("avro schema successfully created from Specific Record");
         } catch (IOException ioException) {
@@ -39,7 +39,7 @@ public class SpecificRecordExample {
         }
 
         //Read from a file
-        final File basicCustomerFile = new File("customer-specific-generated.avro");
+        final File basicCustomerFile = new File("customer-basic-specific-generated.avro");
         final DatumReader<CustomerBasic> datumReader = new SpecificDatumReader<>();
         try (DataFileReader<CustomerBasic> dataFileReader = new DataFileReader<>(basicCustomerFile, datumReader)) {
             while (dataFileReader.hasNext()) {
